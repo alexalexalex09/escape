c07d16720a221ccc8646a59da5f113b8512153f9
Alex <frogitts@gmail.com>
2023-05-24 22:03:14 -0400

    Updated sniper

--- Escape/src/sniper.cpp
+++ Escape/src/sniper.cpp
@@ -23,15 +23,96 @@
 static const int BUILTINLED = 2;
 
 // Pins for the 7-segment display
-#define CLK_PIN 5
-#define DIO_PIN 6
+#define CLK_PIN D5
+#define DIO_PIN D6
+
+// Pins for the buttons
+#define BUTTON_A_PIN D2
+#define BUTTON_B_PIN D3
+
+// Pin for speaker
+#define SOUND_PIN 11
 
 // Delay between frames in milliseconds
 #define FRAME_DELAY 100
 
 const uint8_t circleFrames[] = {SEG_A, SEG_B, SEG_C, SEG_D, SEG_E, SEG_F, SEG_G};
+const uint8_t code[] = {0123, 0012, 0001};
+const uint8_t allON[] = {0xff, 0xff, 0xff, 0xff};
+const uint8_t allOFF[] = {0x00, 0x00, 0x00, 0x00};
 
 TM1637Display display(CLK_PIN, DIO_PIN);
+
+int digitIndex = 1; // Never select the leftmost digit
+int digitValue = 0;
+uint8_t currentDigit[4] = {0, 0, 0, 0};
+
+int codeStage = 0;
+
+// Increases value of selected digit
+void increaseDigit()
+{
+  digitValue++;
+  if (digitValue > 9)
+  {
+    digitValue = 0;
+  }
+}
+
+uint8_t getDigits()
+{
+  return currentDigit[0] * 1000 + currentDigit[1] * 100 + currentDigit[2] * 10 + currentDigit[3];
+}
+
+void updateDisplay()
+{
+  display.showNumberDecEx(digitValue, 0, false, 1, digitIndex); // Display the digit with leading zero
+  currentDigit[digitIndex] = digitValue;                        // Update the digit tracker
+  if (code[codeStage] == getDigits())
+  {                 // Check if the correct code for the current codeStage is being displayed
+    codeStage++;    // If so, advance the stage
+    digitValue = 0; // clear all the numbers
+    for (int i = 0; i < 4; i++)
+    {
+      digitIndex = i;
+      updateDisplay();
+    }
+    if (codeStage = 3) // If that was the last stage, turn every segment on
+    {
+      display.setSegments(allON);
+    }
+    else
+    {
+      digitIndex = codeStage + 1;
+    }
+  }
+}
+
+void handleButtonA()
+{
+  increaseDigit();
+  updateDisplay();
+}
+
+void handleButtonB()
+{
+  digitIndex++;
+  if (digitIndex > 3)
+  {
+    digitIndex = 1;
+  }
+}
+
+void laserSound()
+{
+  for (int i = 0; i < 200; i++)
+  {
+    digitalWrite(SOUND_PIN, HIGH);
+    delayMicroseconds(i);
+    digitalWrite(SOUND_PIN, LOW);
+    delayMicroseconds(i);
+  }
+}
 
 void setup()
 {
@@ -39,13 +120,24 @@ void setup()
 
   // Clear the display
   display.clear();
+
+  pinMode(BUTTON_A_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_B_PIN, INPUT_PULLUP);
+  pinMode(SOUND_PIN, OUTPUT);
 }
 
 void loop()
 {
-  for (uint8_t i = 0; i < sizeof(circleFrames) / sizeof(circleFrames[0]); i++)
+  if (digitalRead(BUTTON_A_PIN) == LOW)
   {
-    display.setSegments(&circleFrames[i], 1, 0);
-    delay(FRAME_DELAY);
+    handleButtonA();
+    delay(100); // Debounce delay
+  }
+
+  // Check if button B is pressed
+  if (digitalRead(BUTTON_B_PIN) == LOW)
+  {
+    handleButtonB();
+    delay(100); // Debounce delay
   }
 }
