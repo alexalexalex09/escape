5b7e6b3a6995e37b1aa8cc443ce4b1f3e0e6d85b
Alex <alexscottbecker@gmail.com>
2023-06-01 19:47:34 -0400

    Version 1 ready

--- Escape/src/sniper.cpp
+++ Escape/src/sniper.cpp
@@ -13,13 +13,12 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 #include <TM1637Display.h>
-#include <vector> // Needed for a dynamically-allocated peer array
 
 /*************************************
- * CUSTOMIZE THIS NUMBER TO SET YOUR CODES
+ * CUSTOMIZE THIS NUMBER TO SET YOUR CODES WITH NO LEADING ZEROES
  */
-const int code[] = {1050, 0050, 0007}; // Different codes beginning with the first stage
-const uint8_t levels = 3;              // Total number of stages
+const int code[] = {1050, 50, 7}; // Different codes beginning with the first stage
+const uint8_t levels = 3;         // Total number of stages
 
 /**
  * @brief PIN number of an LED.
@@ -29,13 +28,13 @@ const uint8_t levels = 3;              // Total number of stages
 static const int BUILTINLED = 2;
 
 // Pins for the 7-segment display
-#define CLK_PIN 4
-#define DIO_PIN 5
+#define CLK_PIN 4 // D2
+#define DIO_PIN 5 // D1
 
 // Pins for the buttons
-#define BUTTON_A_PIN 12
-#define BUTTON_B_PIN 13
-#define BUTTON_C_PIN 14
+#define BUTTON_A_PIN 12 // D6
+#define BUTTON_B_PIN 13 // D7
+#define BUTTON_C_PIN 14 // D5
 
 // Pin for speaker
 #define SOUND_PIN 15
@@ -46,6 +45,7 @@ static const int BUILTINLED = 2;
 const uint8_t circleFrames[] = {SEG_A, SEG_B, SEG_C, SEG_D, SEG_E, SEG_F, SEG_G};
 const uint8_t allON[] = {0xff, 0xff, 0xff, 0xff};
 const uint8_t allOFF[] = {0x00, 0x00, 0x00, 0x00};
+const uint8_t blank[] = {0x00};
 const uint8_t yes[] = {
     SEG_B | SEG_C | SEG_D | SEG_F | SEG_G,            // y
     SEG_A | SEG_D | SEG_E | SEG_F | SEG_G,            // E
@@ -54,7 +54,9 @@ const uint8_t wrong[] = {SEG_G, SEG_G, SEG_G, SEG_G}; // - - - -
 
 TM1637Display display(CLK_PIN, DIO_PIN);
 
-int digitIndex = 1; // Never select the leftmost digit
+int blinkNow = 0; // used to decide whether to display the currently selected number
+int testA = 1;
+int digitIndex = 3; // Never select the rightmost digit
 int digitValue = 0;
 int currentDigit[4] = {0, 0, 0, 0};
 
@@ -76,10 +78,29 @@ void updateDisplay()
   currentDigit[digitIndex] = digitValue;                        // Update the digit tracker
 }
 
+void updateAll()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    digitIndex = i;
+    updateDisplay();
+  }
+}
+
+void refreshAll()
+{
+  for (int i = 0; i < 4; i++)
+  {
+    digitIndex = i;
+    display.showNumberDecEx(currentDigit[digitIndex], 0, false, 1, digitIndex); // Display the digit with leading zero
+  }
+}
+
 void handleButtonA()
 {
   increaseDigit();
   updateDisplay();
+  Serial.println(digitValue);
 }
 
 void handleButtonB()
@@ -87,27 +108,46 @@ void handleButtonB()
   digitIndex++;
   if (digitIndex > 3)
   {
-    digitIndex = 1;
+    digitIndex = 0;
   }
+  digitValue = currentDigit[digitIndex];
+  Serial.println(digitIndex);
 }
 
-uint8_t getDigits()
+int getDigits()
 {
+  Serial.println("****************************");
+  Serial.println(currentDigit[0]);
+  Serial.println(currentDigit[1]);
+  Serial.println(currentDigit[2]);
+  Serial.println(currentDigit[3]);
+  Serial.println(currentDigit[0] * 1000 + currentDigit[1] * 100 + currentDigit[2] * 10 + currentDigit[3]);
+  Serial.println("****************************");
   return currentDigit[0] * 1000 + currentDigit[1] * 100 + currentDigit[2] * 10 + currentDigit[3];
 }
 
 void success()
 {
   display.setSegments(yes);
-  delay(1000);
+  delay(2000);
   display.showNumberDecEx(getDigits(), 0, true, 4, 0); // Use getDigits to get current digits, then display them
 }
 
 void failure()
 {
   display.setSegments(wrong);
-  delay(1000);
+  delay(2000);
   display.showNumberDecEx(getDigits(), 0, true, 4, 0);
+}
+
+void reinitialize()
+{
+  digitIndex = 3;
+  for (int i = 0; i < 4; i++)
+  {
+    currentDigit[i] = 0;
+  }
+  digitValue = 0;
 }
 
 void laserSound()
@@ -126,64 +166,81 @@ void laserSound()
 
 void handleButtonC()
 {
-  if (code[codeStage] == getDigits())
+  int digits = getDigits();
+  Serial.print("Confirming: ");
+  Serial.print(code[codeStage]);
+  Serial.print(" ?= ");
+  Serial.println(digits);
+  if (code[codeStage] == digits)
   {              // Check if the correct code for the current codeStage is being displayed
     codeStage++; // If so, advance the stage
     success();
-    digitValue = 0; // clear all the numbers
-    for (int i = 0; i < 4; i++)
-    {
-      digitIndex = i;
-      updateDisplay();
-    }
+    Serial.print("Yes! New stage:");
+    Serial.println(codeStage);
+    Serial.print("Next answer: ");
+    Serial.println(code[codeStage]);
+    reinitialize();
+    updateAll();
     if (codeStage == levels) // If that was the last stage, turn every segment on
     {
       display.setSegments(allON);
       laserSound();
-    }
-    else
-    {
-      digitIndex = codeStage + 1;
+      codeStage = 0;
     }
   }
   else
   {
+    Serial.println("No");
     failure();
   }
 }
 
 void setup()
 {
+  Serial.begin(115200);
   display.setBrightness(7); // Adjust brightness (0-7)
 
   // Clear the display
   display.clear();
 
-  pinMode(BUTTON_A_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_B_PIN, INPUT_PULLUP);
-  pinMode(BUTTON_C_PIN, INPUT_PULLUP);
+  pinMode(BUTTON_A_PIN, INPUT);
+  pinMode(BUTTON_B_PIN, INPUT);
+  pinMode(BUTTON_C_PIN, INPUT);
   pinMode(SOUND_PIN, OUTPUT);
 }
 
 void loop()
 {
-  if (digitalRead(BUTTON_A_PIN) == LOW)
+  yield();
+  blinkNow++;
+  if (blinkNow == 40000)
+  {
+    display.setSegments(blank, 1, digitIndex);
+  }
+  if (blinkNow == 50000)
+  {
+    blinkNow = 0;
+    int tempIndex = digitIndex;
+    refreshAll();
+    digitIndex = tempIndex;
+  }
+  if (digitalRead(BUTTON_A_PIN) == HIGH)
   {
     handleButtonA();
-    delay(100); // Debounce delay
+    delay(200); // Debounce delay
   }
 
   // Check if button B is pressed
-  if (digitalRead(BUTTON_B_PIN) == LOW)
+  if (digitalRead(BUTTON_B_PIN) == HIGH)
   {
     handleButtonB();
-    delay(100); // Debounce delay
+    delay(200); // Debounce delay
   }
 
   // Check if button B is pressed
-  if (digitalRead(BUTTON_C_PIN) == LOW)
+  if (digitalRead(BUTTON_C_PIN) == HIGH)
   {
     handleButtonC();
-    delay(100); // Debounce delay
+    delay(200); // Debounce delay
   }
 }
