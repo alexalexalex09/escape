8e6c2db1d508826914a7a7cfb802c09fb77fbfb4
Alex <alexscottbecker@gmail.com>
2023-06-04 21:56:13 -0400

    Successfully lights up an LED on correct code entered

--- rpiKeyboardCode/keyboardCode.py
+++ rpiKeyboardCode/keyboardCode.py
@@ -5,14 +5,15 @@
 import sys
 import evdev
 import RPi.GPIO as GPIO
+from time import sleep
 
 # Configure the GPIO pin for the diode
-DIODE_PIN = 17
+DIODE_PIN = 4
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIODE_PIN, GPIO.OUT)
+GPIO.setup(DIODE_PIN, GPIO.OUT, initial=GPIO.LOW)
 
 # Define the keyboard input code to trigger the diode
-TARGET_CODE = "493759"
+TARGET_CODE = "439759"
 
 # Function to search for an item in a list
 def search_item(item, my_list):
@@ -60,15 +61,19 @@ def handle_keyboard_events():
                         GPIO.output(DIODE_PIN, GPIO.HIGH)
                         input_buffer = ""
                         print("Success!")
+                        sleep(5)
+                        GPIO.output(DIODE_PIN, GPIO.LOW)
 
                 else:
                     # Append the pressed key to the input buffer
                     input_buffer += key_value
                     print("Buffer: " + input_buffer)
 
+
 # Run the program
 try:
     handle_keyboard_events()
 except KeyboardInterrupt:
     # Clean up the GPIO pin on program exit
+    GPIO.output(DIODE_PIN, GPIO.LOW)
     GPIO.cleanup()
