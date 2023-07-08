1f2ba6d3784eeed8c2f0e24f941c3e30c0a96559
Alex <alexscottbecker@gmail.com>
2023-07-08 19:20:50 -0400

    Added sounds to keyboardCode

--- symbols/keyboardCode.py
+++ symbols/keyboardCode.py
@@ -6,14 +6,37 @@ import sys
 import evdev
 import RPi.GPIO as GPIO
 from time import sleep
+import pygame
 
 # Configure the GPIO pin for the diode
 DIODE_PIN = 4
+SPEAKER_PIN = 17
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIODE_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(SPEAKER_PIN, GPIO.OUT)  # Replace 17 with the GPIO pin connected to the key
+
+def play_tone(frequency, duration):
+    pwm = GPIO.PWM(SPEAKER_PIN, frequency)
+    pwm.start(50)  # 50% duty cycle
+    sleep(duration)
+    pwm.stop()
+
+def play_arpeggio(duration): # Plays C E G C G E C
+    play_tone(261.63, duration/7)
+    play_tone(329.63, duration/7)
+    play_tone(392, duration/7)
+    play_tone(523.25, duration/7)
+    play_tone(392, duration/7)
+    play_tone(329.63, duration/7)
+    play_tone(261.63, duration/7)
+
+def play_low_c(duration):
+    play_tone(130.81, duration/3)
+    play_tone(65.41, duration/3)
+    play_tone(65.41, duration/3)
 
 # Define the keyboard input code to trigger the diode
-TARGET_CODE = "459735"
+TARGET_CODE = "123425"
 
 # Function to search for an item in a list
 def search_item(item, my_list):
@@ -54,11 +77,13 @@ def handle_keyboard_events():
                     if input_buffer != TARGET_CODE:
                         # Turn off the diode and clear the input buffer
                         GPIO.output(DIODE_PIN, GPIO.LOW)
+                        play_low_c(1)
                         input_buffer = ""
                         print("Code cleared. Diode turned off.")
                     else:
                         # Clear the input buffer without turning off the diode
                         GPIO.output(DIODE_PIN, GPIO.HIGH)
+                        play_arpeggio(3)
                         input_buffer = ""
                         print("Success!")
                         sleep(5)
