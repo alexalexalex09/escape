42d370305c8910d45c1b8753b814be8b43c8b89e
Alex <alexscottbecker@gmail.com>
2023-07-22 10:00:12 -0400

    Add disguise project

--- symbols/keyboardCode.py
+++ symbols/keyboardCode.py
@@ -6,7 +6,7 @@ import sys
 import evdev
 import RPi.GPIO as GPIO
 from time import sleep
-import pygame
+import readchar
 
 # Configure the GPIO pin for the diode
 DIODE_PIN = 4
@@ -15,6 +15,17 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(DIODE_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(SPEAKER_PIN, GPIO.OUT)  # Replace 17 with the GPIO pin connected to the key
 
+# Notes for sound
+note_G = 391.00
+note_A = 440.00
+note_B = 493.88
+note_C = 523.25
+note_D = 587.33
+note_E = 659.25
+note_Fs = 739.99
+note_Go = 790.00
+
+
 def play_tone(frequency, duration):
     pwm = GPIO.PWM(SPEAKER_PIN, frequency)
     pwm.start(50)  # 50% duty cycle
@@ -22,18 +33,32 @@ def play_tone(frequency, duration):
     pwm.stop()
 
 def play_arpeggio(duration): # Plays C E G C G E C
-    play_tone(261.63, duration/7)
-    play_tone(329.63, duration/7)
-    play_tone(392, duration/7)
-    play_tone(523.25, duration/7)
-    play_tone(392, duration/7)
-    play_tone(329.63, duration/7)
-    play_tone(261.63, duration/7)
+    q = duration/6
+    breath = .001
+    play_tone(note_G, q*.75)
+    sleep(breath)
+    play_tone(note_C, q*.25)
+    sleep(breath)
+    play_tone(note_C, q*1.5)
+    sleep(q*.25)
+    play_tone(note_C, q*.25)
+    sleep(breath)
+    play_tone(note_D, q*.33)
+    sleep(breath)
+    play_tone(note_C, q*.33)
+    sleep(breath)
+    play_tone(note_D, q*.33)
+    sleep(breath)
+    play_tone(note_E, q*.75)
+    sleep(breath)
+    play_tone(note_Go, q*.25)
+    sleep(breath)
+    play_tone(note_Go, q)
 
 def play_low_c(duration):
+    play_tone(262.00, duration/3)
     play_tone(130.81, duration/3)
-    play_tone(65.41, duration/3)
-    play_tone(65.41, duration/3)
+    play_tone(130.81, duration/3)
 
 # Define the keyboard input code to trigger the diode
 TARGET_CODE = "123425"
@@ -63,36 +88,33 @@ def handle_keyboard_events():
 
     print("Waiting for input...")
     input_buffer = ""
-    for event in keyboard.read_loop():
-        if event.type == evdev.ecodes.EV_KEY:
-            key_event = evdev.categorize(event)
-            if key_event.keystate == key_event.key_down:
-                key_value = key_event.keycode.split("_")[-1]
-                kp = key_value.startswith('KP')
-                if (kp):
-                    key_value = key_value[2:]
-                print(key_value + " entered.")
-                if key_value == "ENTER":
-                    # Check if the input code matches the target code
-                    if input_buffer != TARGET_CODE:
-                        # Turn off the diode and clear the input buffer
-                        GPIO.output(DIODE_PIN, GPIO.LOW)
-                        play_low_c(1)
-                        input_buffer = ""
-                        print("Code cleared. Diode turned off.")
-                    else:
-                        # Clear the input buffer without turning off the diode
-                        GPIO.output(DIODE_PIN, GPIO.HIGH)
-                        play_arpeggio(3)
-                        input_buffer = ""
-                        print("Success!")
-                        sleep(5)
-                        GPIO.output(DIODE_PIN, GPIO.LOW)
+    while(True):
+        print('Reading a key:')
+        key_value = readchar.readkey()
+        print(key_value + " entered.")
+        if key_value == "ENTER":
+            # Check if the input code matches the target code
+            if input_buffer != TARGET_CODE:
+                # Turn off the diode and clear the input buffer
+                GPIO.output(DIODE_PIN, GPIO.LOW)
+                play_low_c(1)
+                input_buffer = ""
+                print("Code cleared. Diode turned off.")
+            else:
+                # Clear the input buffer without turning off the diode
+                GPIO.output(DIODE_PIN, GPIO.HIGH)
+                play_arpeggio(3)
+                input_buffer = ""
+                print("Success!")
+                sleep(1)
+                GPIO.output(DIODE_PIN, GPIO.LOW)
 
-                else:
-                    # Append the pressed key to the input buffer
-                    input_buffer += key_value
-                    print("Buffer: " + input_buffer)
+        else:
+            # Append the pressed key to the input buffer
+            input_buffer += key_value
+            print("Buffer: " + input_buffer)
+
+
 
 
 # Run the program
@@ -102,3 +124,5 @@ except KeyboardInterrupt:
     # Clean up the GPIO pin on program exit
     GPIO.output(DIODE_PIN, GPIO.LOW)
     GPIO.cleanup()
+
+
